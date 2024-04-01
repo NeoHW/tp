404666c806ff9c1d35685d13ced00ba647219d94
guohuang <guohuang88@gmail.com>
2024-04-01 18:06:44 +0800

    Add logs for DeleteCommand and DeleteCommandParser

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,7 +1,12 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -11,14 +16,21 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class DeleteCommandParser implements Parser<DeleteCommand> {
 
+    private static final Logger logger = LogsCenter.getLogger(DeleteCommandParser.class);
+
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns a DeleteCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
+        logger.log(Level.INFO, "Received arguments: " + args + " for DeleteCommand; Attempting to parse..");
+        requireNonNull(args);
+
         try {
             Index index = ParserUtil.parseIndex(args);
+            logger.log(Level.INFO, "Index is valid.");
+
             return new DeleteCommand(index);
         } catch (ParseException pe) {
             throw new ParseException(
