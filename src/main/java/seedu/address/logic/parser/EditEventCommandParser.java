ca6953cad453b74c80d1a1f8fe5e18f8e549a9e8
guohuang <guohuang88@gmail.com>
2024-04-01 11:52:47 +0800

    Add assert and log statements for EditEventCommand

--- src/main/java/seedu/address/logic/parser/EditEventCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditEventCommandParser.java
@@ -7,6 +7,10 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.ParserUtil.arePrefixesPresent;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.EditEventCommand;
@@ -17,7 +21,7 @@ import seedu.address.model.patient.Event;
  * Parses the user's input arguments and creates a new EditEventCommand
  */
 public class EditEventCommandParser implements Parser<EditEventCommand> {
-
+    private static final Logger logger = LogsCenter.getLogger(EditEventCommandParser.class);
     /**
      * Returns a new EditEventCommand instance.
      * @param args User's input.
@@ -25,20 +29,19 @@ public class EditEventCommandParser implements Parser<EditEventCommand> {
      * @throws ParseException If the user input does not conform to the expected format.
      */
     public EditEventCommand parse(String args) throws ParseException {
+        logger.log(Level.INFO, "Received arguments: " + args + " for EditEventCommand; Attempting to parse..");
         requireNonNull(args);
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_EVENT,
-                PREFIX_NAME, PREFIX_DATETIME);
 
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_EVENT, PREFIX_NAME, PREFIX_DATETIME);
         if (!arePrefixesPresent(argMultimap, PREFIX_EVENT, PREFIX_NAME, PREFIX_DATETIME)
                 || argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     EditEventCommand.MESSAGE_USAGE));
         }
+        logger.log(Level.INFO, "All prefixes required are present.");
 
         Index patientIndex;
         Index eventIndex;
-        Event event;
-
         try {
             patientIndex = ParserUtil.parseIndex(argMultimap.getPreamble());
             eventIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_EVENT).get());
@@ -46,9 +49,11 @@ public class EditEventCommandParser implements Parser<EditEventCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     EditEventCommand.MESSAGE_USAGE), e);
         }
+        logger.log(Level.INFO, "Patient Index and Event Index are valid.");
 
-        event = ParserUtil.parseEvent(argMultimap.getValue(PREFIX_NAME).get(),
+        Event event = ParserUtil.parseEvent(argMultimap.getValue(PREFIX_NAME).get(),
                 argMultimap.getValue(PREFIX_DATETIME).get());
+        logger.log(Level.INFO, "All arguments are valid.");
 
         return new EditEventCommand(patientIndex, eventIndex, event);
     }
