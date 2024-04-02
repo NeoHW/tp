74dfb62f7c1774dcae32acc4821cb1487ee8c31c
Zhi Wei <tanzhiwei7@gmail.com>
2024-04-02 21:52:55 +0800

    Model: Updates Model to support sort command
    Add new NameComparator class to compare patients by name.
    
    Add new PreferredNameComparator class to compare patients by preferred
    name.
    
    Add new Model#getFullPatientList() method to get unmodifiable view of
    the full patient list.
    
    Add new Model#updatePatientList(List<Patient>) method to update the
    patient list with an updated patient list.
    
    Add new ModelManager#getFullPatientList() to get the full patient list.
    
    Add new ModelManager#updatePatientList(List<Patient>) to updated the
    patient list with an updated patient list.
    
    Update the ModelStub in AddCommandTest to override new methods added in
    Model Interface.

---
+++ src/main/java/seedu/address/model/patient/comparators/PreferredNameComparator.java
@@ -0,0 +1,35 @@
+package seedu.address.model.patient.comparators;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Comparator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.patient.Patient;
+
+/**
+ * Comparator for a Patient's preferred name in PatientSync.
+ */
+public class PreferredNameComparator implements Comparator<Patient> {
+    public static final PreferredNameComparator PREFERRED_NAME_COMPARATOR = new PreferredNameComparator();
+    private static final Logger logger = LogsCenter.getLogger(PreferredNameComparator.class);
+
+    @Override
+    public int compare(Patient patientOne, Patient patientTwo) {
+        logger.log(Level.INFO, "Attempting to compare patients by preferred name");
+
+        requireAllNonNull(patientOne, patientTwo);
+
+        // Comparing patients via preferred name
+        return patientOne.getPreferredName().toString()
+                .compareToIgnoreCase(patientTwo.getPreferredName().toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Comparator for Patients by Patients' preferred name in alphanumerical order.";
+    }
+
+}
